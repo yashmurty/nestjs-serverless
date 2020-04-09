2fabef9b10253d582974b8323d2aea215426365d
Yash Murty <yashmurty@gmail.com>
2020-04-09 17:57:21 +0900

    :construction: Add config package

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
