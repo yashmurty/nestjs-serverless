9a8538b146e3fae01a70994218590abae250877f
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2020-04-09 19:14:00 +0900

    :dog: Add DocumentBuilder to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 
@@ -6,6 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const configService = app.get(ConfigService)
   const backendAppPort = configService.get('BACKEND_APP_PORT')
+
+  const options = new DocumentBuilder()
+    .setTitle('nestjs-serverless')
+    .setDescription('SwaggerUI for nestjs-serverless API')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('swagger', app, document)
 
   await app.listen(backendAppPort)
 }
